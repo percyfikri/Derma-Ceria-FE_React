d14470ed6be195d2f8d1440b813c5e2a6a57089b
percyfikri <mafikri27@gmail.com>
2024-06-27 00:07:38 +0700

    update : PayDonasiUang (path image)

--- Derma-Ceria/src/components/PopUpPemilihanPembayaranUang/ModalPembayaranUang.jsx
+++ Derma-Ceria/src/components/PopUpPemilihanPembayaranUang/ModalPembayaranUang.jsx
@@ -4,14 +4,19 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import styles from './PopUpPemilihanPembayaran.module.css';
 
+import gopay from '../../assets/logos/GoPay.png';
+import dana from '../../assets/logos/Dana.png';
+import briva from '../../assets/logos/BRIVA.png';
+import mandiri from '../../assets/logos/Mandiri.png';
+
 const ModalPembayaranUang = ({ showModal, handleClose, donationAmount, serviceFee, onSuccess }) => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
 
   const buktiMethods = [
-    { id: 1, name: 'GoPay', image: './src/assets/logos/GoPay.png', title: 'E-Wallet' },
-    { id: 2, name: 'Dana', image: './src/assets/logos/Dana.png', title: 'E-Wallet' },
-    { id: 3, name: 'BRIVA', image: './src/assets/logos/BRIVA.png', title: 'Virtual Account' },
-    { id: 4, name: 'Mandiri', image: './src/assets/logos/Mandiri.png', title: 'Virtual Account' }
+    { id: 1, name: 'GoPay', image: gopay, title: 'E-Wallet' },
+    { id: 2, name: 'Dana', image: dana, title: 'E-Wallet' },
+    { id: 3, name: 'BRIVA', image: briva, title: 'Virtual Account' },
+    { id: 4, name: 'Mandiri', image: mandiri, title: 'Virtual Account' }
   ];
 
   const groupedBuktiMethods = buktiMethods.reduce((acc, method) => {
